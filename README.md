13cf35e0146605ff8636e05770f0230e8cda5f17
Jirka Manak <jirka.manak@email.cz>
2020-04-04 18:33:34 +0200

    funguje pohyb a hranice

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ Testování matplotlib a nástřel classy Person.
 Matplot možná nebude nejlepší volbou pro takové kvantum dat... už s tou jednou rozbitou a špatně naprogramovanou tečkou to neběží tak rychle, jak by mělo a žere mi to 70% cpu. Teoreticky by šlo nechat simulaci běžet a zobrazovat jen třeba každý 100stav nebo nemít živý výstup vůbec.
 
 Jde se vyzkoušet manim...
+
+### 4.4
+Fungují základní příklady ale nejede shader branch protože nemám grafiku.
+
+Zpátky k matplotlib...
+Funguje základní pohyb a respektování hranic.
+Už mám i vizi výstupu.
+
+Přidán requirements.txt
--- sim.py
+++ sim.py
@@ -4,56 +4,123 @@ import numpy as np
 import numpy.random as rnd
 import math
 
-WIDTH = 1000
-HEIGHT = 1000
-MAX_SPEED = 50
+MAX_SPEED = 10
+
+BOUND = 500
 
 #plt.style.use('fivethirtyeight')
 #plt.tight_layout()
 
+class World():
+    def __init__(self):
+        self.people = []
+    
+    def add_person(self, person):
+        self.people.append(person)
+
+    def get_location_of_all(self):
+        return ([p.location.x for p in self.people], [p.location.y for p in self.people])
+
+
+def rand_float_in_range(lower, upper):
+    return (rnd.random(1) * (upper - lower)) + lower
+
+def rand_degrees():
+    return rnd.randint(0, 360)
+
+
+class Coordinates():
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    
+    def is_within_bound(self, bound):
+        if bound.x > abs(self.x) and bound.y > abs(self.y):
+            return True
+        return False
+
+    def set_random_within_bound(self, bound):
+        self.x = rnd.randint(-bound.x, bound.x)
+        self.y = rnd.randint(-bound.y, bound.y)
+
 class Person():
     def __init__(self):
-        self.x = rnd.randint(0, WIDTH)
-        self.y = rnd.randint(0, HEIGHT)
-        self.direction = rnd.randint(0, 360)
-        self.dir_holdoff = rnd.randint(0, 50)
-        self.speed = MAX_SPEED * rnd.random(1)
+        self.center = Coordinates(0, 0)
+        self.bound = Coordinates(BOUND, BOUND)
+        self.max_speed = MAX_SPEED
+
+        self.location = Coordinates()
+        self.location.set_random_within_bound(self.bound)
+        self.change_mind()
+        
     
-    def step(self):
-        if self.direction >= 360:
-            self.direction -= 360
+    def change_mind(self):
+        self.heading = rand_degrees()
+        self.speed = rand_float_in_range(0, self.max_speed)
+        self.change_holdoff = rnd.randint(10, 50)
+    
+    
+    def update(self):
+        if self.change_holdoff > 0:
+            self.change_holdoff -= 1
+            if self.change_holdoff == 0:
+                self.change_mind()
+                """ self.heading += 45
+                if self.heading > 360:
+                   self.heading -= 360
+                print (self.heading)
+                self.change_holdoff = 10 """
         
-        if self.dir_holdoff > 0:
-            #self.dir_holdoff -= 1
-            if self.dir_holdoff == 0:
-               self.dir_holdoff = rnd.randint(0, 50)
-               self.direction = rnd.randint(0, 360)
-               self.speed = MAX_SPEED * rnd.random(1)
-        
-        self.x += math.sin(math.radians(self.direction))
-        self.y += math.cos(math.radians(self.direction))
+        self.location.x += math.cos(math.radians(self.heading)) * self.speed
+        self.location.y += math.sin(math.radians(self.heading)) * self.speed
 
-        if self.x > WIDTH:
-            self.x -= WIDTH - self.x
-            self.direction = rnd.randint(90, 270)
-        if self.x < 0:
-            self.x = -self.x
-            self.direction = rnd.randint(270, 450)
-        
-        if self.y > HEIGHT:
-            self.y -= HEIGHT - self.y
-            self.direction = rnd.randint(180, 360)
-        if self.y < 0:
-            self.y = -self.y
-            self.direction = rnd.randint(0, 180)
+        if not self.location.is_within_bound(self.bound):
+            self.heading = self.get_heading_to_center()
+            print(self.heading)
 
-p = Person()
 
-def field(i):
+    def get_heading_to_center(self):
+        # draw it on a piece of paper to see that it works (compare to get_theta)
+        x, y = -self.location.x, -self.location.y
+        deg = math.degrees(math.asin(abs(y) / math.sqrt(x ** 2 + y ** 2)))
+        if x < 0 and y > 0:
+            deg = 180 - deg
+        elif x < 0 and y < 0:
+            deg += 180
+        elif x > 0 and y < 0:
+            deg = 360 - deg
+        return deg
+    
+    def get_theta(self):
+        x, y = self.location.x, self.location.y
+        # vector direction
+        deg = math.degrees(math.asin(abs(y) / math.sqrt(x ** 2 + y ** 2)))
+        if x < 0 and y > 0:
+            deg = 180 - deg
+        elif x < 0 and y < 0:
+            deg += 180
+        elif x > 0 and y < 0:
+            deg = 360 - deg
+        return deg
+
+
+world = World()
+for i in range(1000):
+    world.add_person(Person())
+
+def plot_field():
     plt.cla()
-    plt.scatter(p.x, p.y, alpha=0.7)
-    p.step()
+    x, y = world.get_location_of_all()
+    #print(f"x:{x}\ny:{y}\n\n")
+    plt.scatter(x, y, alpha=0.7)
+    plt.xlim(-BOUND, BOUND)
+    plt.ylim(-BOUND, BOUND)
 
-ani = fanim(plt.gcf(), field, interval = 10)
+def field_update(i):
+    plot_field()
+    for p in world.people:
+        p.update()
+
+ani = fanim(plt.gcf(), field_update, interval = 100)
 
 plt.show()
